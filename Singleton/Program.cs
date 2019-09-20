8a024b2d200dd3fa910648f91aee35732d9432a4
RobertGarcia1020 <garcia.robert1020@gmail.com>
2019-09-20 17:58:01 -0500

    Se añade Singleton
    Se añade patron Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Singleton.Singleton;
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PatronSingleton.IntanciaSingleton.mensaje);
+            PatronSingleton.IntanciaSingleton.mensaje = "Hola esta es una prueba";
+            Console.WriteLine(PatronSingleton.IntanciaSingleton.mensaje);
+        }
+    }
+}
---
+++ Singleton/Singleton/PatronSingleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton.Singleton
+{
+    public class PatronSingleton
+    {
+        private static PatronSingleton Instancia;
+        public string mensaje;
+        protected PatronSingleton() {
+            mensaje = "hola mundo";
+        }
+
+        public static PatronSingleton IntanciaSingleton {
+            get {
+
+                if (Instancia == null)
+                    Instancia = new PatronSingleton();
+
+                return Instancia;
+            }
+        }
+    }
+}
